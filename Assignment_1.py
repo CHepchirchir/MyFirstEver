76e9d0d8e095f8355007b82c0273ae2db2fd8397
CHepchirchir <41426751+CHepchirchir@users.noreply.github.com>
2021-04-06 11:50:20 +0300

    Add files via upload

---
+++ Assignment_1.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  6 10:23:32 2021
+
+@author: chepchir
+"""
+
+#name = input('Enter First and last name')
+#print(name)
+#print()
+
+first_name= input('First Name')
+Last_name= input('Last Name')
+Full_names = first_name + Last_name
+print(Full_names)
+
+country = input('enter your home country')
+print(country)
+job= input('what do you do?')
+print(job)
+email = input ('enter your mail address')
+print(email)
+phone =input('whats your mobile number')
+print(phone)
+city= input('enter city of residence')
+print('city')
+address =input('enter your mailing address')
+print(address)
+
