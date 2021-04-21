3f5b0299d9e00c398c076dc897f3f34a6dc98d47
CHepchirchir <41426751+CHepchirchir@users.noreply.github.com>
2021-04-21 11:20:42 +0300

    Update Assignment5_Updatedlast.py

--- Assignment5_Updatedlast.py
+++ Assignment5_Updatedlast.py
@@ -7,13 +7,11 @@ Created on Fri Apr 16 13:23:30 2021
 
 options = '' #Declaring an empty string
 
-#Displaying the options
-
- while options != 'Q'
+while options != 'Q':
           options = input('Enter the options [P,C,NM,M,CM,Y,In,Q]?')
           print()
       
-      if options == 'P':
+if options == 'P':
           print('[P] Print Options')
           print('[C] Convert from Celsius')
           print('[NM] Convert from Nautical Miles')
@@ -21,16 +19,16 @@ options = '' #Declaring an empty string
           print('[CM] Convert from Centimeters')
           print('[Y] Convert from Yards')
           print('[In] Convert from Inches')
-          print ('[Q] Quit')
+          print('[Q] Quit')
           
           print ()
-      elif options == 'C':       
+elif options == 'C':       
           #1. Temperature from Fahrenheit to Celsius and back
           celsius = 37.5
           fahrenheit = celsius * 1.8 + 32
           print('%0.3f farenheit equal to %0.1f Celsius' %(fahrenheit,celsius))
           
-      elif options == 'NM':
+elif options == 'NM':
           #2. Convert Nautical Miles to KMS and back,
           KMS = float(input('Enter distance in KMS: '))
           Nautical= float(input('Enter distance in Nautical miles: '))
@@ -41,7 +39,7 @@ options = '' #Declaring an empty string
           print('%0.3fNautical_Miles equal to %0.3f Kilometers' % (KMS,Nautical_Mile))
           print('%0.3fKilometers equal to %0.3f Nautical Miles' % (Nautical,KILOMETERS))
           
-       elif options == 'M':
+elif options == 'M':
           #3. Convert Kilometer to Miles and back,
           kilo_meter = float(input('Enter distance in Kilometers: '))
           Miles =float(input('Enter distance in Miles: '))
@@ -52,7 +50,7 @@ options = '' #Declaring an empty string
           print('%0.3f kilometers is equal to %0.3f miles' % (Miles,Kilometer))
           print('%0.3f miles is equal to %0.3f Kilometers' % (kilo_meter,miles))
           
-       elif options == 'CM':      
+elif options == 'CM':      
           ## 4.Convert Centimeters to meters and back
           centimeter = float(input("Enter the length in Centimeters: "))
           meter = float(input("Enter the length in Meters: "))
@@ -61,7 +59,7 @@ options = '' #Declaring an empty string
           print('%0.3f Centimeters is equal to %0.3f Meters' % (meter ,Centimeters))
           print('%0.3f Centimeters is equal to %0.3f meters' % (centimeter,meters))
           
-       elif options == 'Y':
+elif options == 'Y':
           ## 5Convert Yard to Meters and back,
           meter = float(input("Enter the length in Meters: "))
           yard =float(input('Enter lenght in yards:'))
@@ -72,7 +70,7 @@ options = '' #Declaring an empty string
           print("%d Meter is equal to %.4f Yard" % (meter, meter_to_yard))
           print("%d Yard is equalto %.4f Meter" % (yard, yard_to_meter))
 
-       elif options == 'In':
+elif options == 'In':
           #6. Convert Inches to Centimeters and back
           inches= float(input("Enter length in Inches: "))
           centimeter = float(input("Enter the length in Centimeters: "))
@@ -83,10 +81,11 @@ options = '' #Declaring an empty string
           print("%d Inches is equal to %.4f Centimeters" % (inches, Inches))
           print("%d Centimeters is equalto %.4f inches" % (centimeter, Centimeters))
         
-        elif option =='Q':
-        break
- else:
-    print("invalid option")
+elif options =='Q':
+          print ('Quit')
+else:
+          print("invalid option")
     
 print('Thank you')
+       
        
